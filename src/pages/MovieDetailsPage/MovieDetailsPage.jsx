c69111dd9f192c2817906bf9bbcde9d7581e7d89
12DaNa21 <145206474+12DaNa21@users.noreply.github.com>
2024-04-30 21:17:36 +0300

    what in commit

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,36 +1,74 @@
-import { useEffect, useState } from "react";
-// import css from "./HomePage.module.css";
-import MovieList from "../../components/MovieList/MovieList"
-import { getPopularMovies } from "../../movies-api";
+import { Link, Outlet, useLocation, useParams } from 'react-router-dom';
+import { Suspense, useEffect, useRef, useState } from 'react';
+import css from './MovieDetailsPage.module.css';
+import { getMovieByid } from '../../movies-api';
+export default function MovieDetailsPage() {
+  const { movieId } = useParams();
+  const [movie, setMovie] = useState(null);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-export default function HomePage(){
-    const [movies, setMovies] =useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
+  const location = useLocation();
+  const backLinkURLRef = useRef(location.state ?? '/');
 
-    useEffect(() => {
-        async function fetchMovies(){
-            try {
-                setLoading(true);
-                const data = await getPopularMovies();
-                setMovies(data);
-            } catch (error) {
-                setError(true)
-            } finally {
-                setLoading(false)
-            }
-        }
-        fetchMovies()
-    }, [])
+  useEffect(() => {
+    async function fetchMovieById() {
+      try {
+        setError(false);
+        setLoading(true);
+        const data = await getMovieByid(movieId);
+        setMovie(data);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchMovieById();
+  }, [movieId]);
 
-    return(
-        <>
-        <h2>Trending today</h2>
+  return (
+    <div >
+      <Link to={backLinkURLRef.current} className={css.backLink}>
+        Go back
+      </Link>
+      {loading && <b>Loading information about movie</b>}
+      {error && <b>Sorry, we have some troubles</b>}
+      {movie && (
+        <div className={css.container}>
+          <img
+            src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
+            alt="movie poster"
+          />
+          <div>
+            <h2>{movie.original_title}</h2>
+            <p>User score: {movie.vote_average * 10}%</p>
+            <h2>Overview</h2>
+            <p>{movie.overview}</p>
 
-        {loading && <b>Loading payments...</b>}
-        {error && <b>Sorry, we hame some troubles</b>}
-        {movies.length > 0 && <MovieList data={movies}/>}
+            <h2>Genres</h2>
+            <ul>
+              {movie.genres.map(el => (
+                <li key={el.id}>{el.name}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
+      <hr />
+      <p>Additional information</p>
+      <ul>
+        <li>
+          <Link to="cast">Cast</Link>
+        </li>
+        <li>
+          <Link to="reviews">Reviews</Link>
+        </li>
+      </ul>
 
-        </>
-    )
+      <Suspense fallback={<b>Loading nested route...</b>}>
+        <Outlet />
+      </Suspense>
+    </div>
+  );
 }
